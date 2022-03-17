cbb8b7344ff8a9c8d589f5614044d8372db423ab
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-03-16 20:47:18 -0400

    commit tree 92934992235a2cafbaf22e82fc431d4c6b6b88c0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ('Mr. Chocoloate', 'Male');
+INSERT INTO bears (name, sex) VALUES ('Rowdy', 'Male');
+INSERT INTO bears (name, sex) VALUES ('Tabitha', 'Female');
+INSERT INTO bears (name, sex) VALUES ('Sergeant Brown', 'Male');
+INSERT INTO bears (name, sex) VALUES ('Melissa', 'Female');
+INSERT INTO bears (name, sex) VALUES ('Grinch', 'Male');
+INSERT INTO bears (name, sex) VALUES ('Wendy', 'Female');
+INSERT INTO bears (name, sex) VALUES (NULL, 'Male');
